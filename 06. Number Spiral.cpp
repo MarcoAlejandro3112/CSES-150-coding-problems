a56d0a504e5121b5d5e9d156d69da73489451d98
Marco Altamirano <maaltamiranohiga@gmail.com>
2020-07-30 14:42:56 -0500

    06. Number Spiral

---
+++ 06. Number Spiral.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while(n--){
+        int x,y;
+        cin >> x >> y;
+        if(x == y){
+            cout << (x*x)-(x-1);
+        } else if(x > y){
+            if(x % 2 == 0){
+                cout << x*x - (y-1);
+            } else {
+                cout << x*x -(x+(x-y-1));
+            }
+        } else{
+            if(y%2 == 0){
+                cout << y*y -(y+(y-x-1));
+            } else {
+                cout << y*y -(x-1);
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
